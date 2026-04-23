a46eb077c7a0e66016694a60fa209c75d9b874ae
nadaelbehiry <nadaelbehiry50@gmail.com>
2026-04-23 06:18:59 +0200

    login update and creating forgotpassword page & resetpassword page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
 import About from "./pages/common/About";
 import Contact from "./pages/common/Contact";
 import Pricing from "./pages/common/Pricing";
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/pricing" element={<Pricing />} />
